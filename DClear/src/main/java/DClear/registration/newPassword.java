71dfefba9b1e20aa79bb4e1cf906bd89275fb469
Arunkumarr <arunr90085@gmail.com>
2023-08-19 15:06:39 +0530

    f updated

---
+++ DClear/src/main/java/DClear/registration/newPassword.java
@@ -0,0 +1,53 @@
+package DClear.registration;
+
+
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/newPassword")
+public class newPassword extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		HttpSession session = request.getSession();
+		String newPassword = request.getParameter("password");
+		String confPassword = request.getParameter("confPassword");
+		RequestDispatcher dispatcher = null;
+		if (newPassword != null && confPassword != null && newPassword.equals(confPassword)) {
+
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				Connection con = DriverManager.getConnection("jdbc:mysql://dclear.c6oihiaxexbs.eu-north-1.rds.amazonaws.com/dclear","arunkumar","Arunr!90085");
+				PreparedStatement pst = con.prepareStatement("update tutor_user set password= ? where email = ? ");
+				pst.setString(1, confPassword);
+				pst.setString(2, (String) session.getAttribute("email"));
+
+				int rowCount = pst.executeUpdate();
+				if (rowCount > 0) {
+					request.setAttribute("status", "resetSuccess");
+					dispatcher = request.getRequestDispatcher("tutorlogin.jsp");
+				} else {
+					request.setAttribute("status", "resetFailed");
+					dispatcher = request.getRequestDispatcher("forgotPassword.jsp");
+				}
+				dispatcher.forward(request, response);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
